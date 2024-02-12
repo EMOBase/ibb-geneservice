88e442aea26b4e9064313704926997b202ac7720
Canh Nguyen Duc <cnguyen2@biologie.uni-goettingen.de>
2024-02-12 18:29:20 +0100

    Initialize project

---
+++ src/test/java/ibb/api/geneservice/PhenotypeEndpointsIT.java
@@ -0,0 +1,8 @@
+package ibb.api.geneservice;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class PhenotypeEndpointsIT extends PhenotypeEndpointsTest {
+    // Execute the same tests but in packaged mode.
+}
